755470f218e042bec4676d36eb31754ca15098bd
Jahongir Xo'jamuratov <73906475+Jahongir7@users.noreply.github.com>
2023-06-07 09:38:07 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
         <Tab eventKey="rsa" title="RSA">
           <RSAContainer />
         </Tab>
-        <Tab eventKey="decrypt" title="Deshifrlash"></Tab>
       </Tabs>
     </div>
   );
